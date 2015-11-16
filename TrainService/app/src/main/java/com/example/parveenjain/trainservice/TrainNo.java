4b3e1c7d09585cee7c16da2e7e01cf7e34d5cc9e
prateekj01234 <prateekj01234@gmail.com>
2015-11-17 01:23:58 +0530

    correction

--- TrainService/app/src/main/java/com/example/parveenjain/trainservice/TrainNo.java
+++ TrainService/app/src/main/java/com/example/parveenjain/trainservice/TrainNo.java
@@ -6,26 +6,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.support.v4.app.FragmentActivity;
 
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class StationsMap extends AppCompatActivity {
+public class TrainNo extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stations_map);
-        GoogleMap googleMap;
-        googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        setContentView(R.layout.activity_train_no);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
